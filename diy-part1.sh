87375a3840a9855019ee8757bb33f0308a1d690e
sayhello023 <61860033+sayhello023@users.noreply.github.com>
2020-06-18 03:35:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,6 +12,4 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-sed -i '$a src-git clone https://github.com/AdguardTeam/AdGuardHome' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
